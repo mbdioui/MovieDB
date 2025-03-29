325f6068031ae75769ea9dbb735508cb52f2184d
salah bdioui <bdiouimedsalah@gmail.com>
2025-03-29 15:02:23 +0100

    init project IDE

---
+++ app/src/main/java/com/neopixel/moviesearch/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.neopixel.moviesearch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintSet
+import com.neopixel.moviesearch.ui.theme.MovieSearchTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val snackbarHostState = remember { SnackbarHostState() }
+            val coroutineScope = rememberCoroutineScope()
+            val scrollState = rememberScrollState(0)
+            MovieSearchTheme {
+                Scaffold(modifier = Modifier.fillMaxSize(), snackbarHost = {
+                    SnackbarHost(hostState = snackbarHostState)
+                }) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .verticalScroll(state = scrollState),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Greeting(
+                            name = "Android", modifier = Modifier.padding(innerPadding)
+                        )
+                        Spacer(modifier = Modifier.height(30.dp))
+                        Button(
+                            modifier = Modifier.fillMaxWidth(0.5f),
+                            onClick = {
+                                coroutineScope.launch {
+                                    snackbarHostState.showSnackbar(
+                                        message = "hello snackbar",
+                                        actionLabel = "click me",
+                                        duration = SnackbarDuration.Long,
+                                    )
+                                }
+                            }) {
+                            Text("hello click me plz")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!", modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MovieSearchTheme {
+        Greeting("Android")
+    }
+}
